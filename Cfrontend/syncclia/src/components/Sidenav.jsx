499826faf1a0e33675def4302d067b00ddaee882
Charity Jelimo <83470048+jelimo-charity@users.noreply.github.com>
2023-06-26 12:41:08 +0000

    frontend syncclia

--- Cfrontend/syncclia/src/components/Sidenav.jsx
+++ Cfrontend/syncclia/src/components/Sidenav.jsx
@@ -1,16 +1,30 @@
+import { useContext } from 'react'
 import './sidenav.css'
 import {FaUser} from 'react-icons/fa'
+import { Context } from '../context/actionContext/Context'
+
+
 function Sidenav() {
+  const { dispatch } = useContext(Context)
+  const handleProfile = () => {
+    dispatch({ type: "PROFILE", payload: "profile"})
+  }
+  const handleAdd = () => {
+    dispatch({ type: "ADD", payload: "add"})
+  }
+  const handleView = () => {
+    dispatch({ type: "VIEW", payload: "view"})
+  }
   return (
     <div>
         <div className="sidenav">
             <div className="sidenav-wrapper">
-                <div className="sidenav-header"> <FaUser/>My Profile </div>
+                <div className="sidenav-header" onClick={handleProfile}> <FaUser/>My Profile </div>
             </div>
             <div className="sidenav-wrapper">
-                <div className="sidenav-item"> Add Action </div> 
+                <div className="sidenav-item" onClick={handleAdd}> Add Action </div> 
             </div>
-            <div className="sidenav-item"> View Actions </div>
+            <div className="sidenav-item" onClick={handleView}> View Actions </div>
         </div>
       
     </div>
